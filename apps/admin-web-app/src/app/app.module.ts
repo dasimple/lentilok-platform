c6b90387cda04c07fb18e1ad5642ca0c27030758
dasimple <dasimple.com@gmail.com>
2021-08-12 02:13:32 +0300

    added @angular/pwa to admin app

--- apps/admin-web-app/src/app/app.module.ts
+++ apps/admin-web-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +12,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule
